974b756ed7d861ec7b55491623ec2974d6b91e4f
TimotheGonin <gonin.timothe@gmail.com>
2023-03-22 11:49:57 +0100

    buyTablet

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 //ACTION
 
 const BUY_PHONE = "BUY_PHONE";
+const BUY_TABLET = "BUY_TABLET";
 
 function buyPhone() {
 	return {
 		type: BUY_PHONE,
+	};
+}
+
+function buyTablet() {
+	return {
+		type: BUY_TABLET,
 	};
 }
 
@@ -13,6 +20,7 @@ function buyPhone() {
 
 const initialState = {
 	phones: 5,
+	tablets: 10,
 };
 
 const reducer = (state = initialState, action) => {
@@ -22,7 +30,11 @@ const reducer = (state = initialState, action) => {
 				...state,
 				phones: state.phones - 1,
 			};
-
+		case BUY_TABLET:
+			return {
+				...state,
+				tablets: state.tablets - 1,
+			};
 		default:
 			return state;
 	}
@@ -34,15 +46,21 @@ const store = Redux.createStore(reducer);
 
 //Récupérer la data du Store
 const availablePhones = document.getElementById("count");
+const availableTablets = document.getElementById("count-tab");
 availablePhones.innerHTML = store.getState().phones;
+availableTablets.innerHTML = store.getState().tablets;
 
 //Effectuer le Dispatch d'un action
 document.getElementById("buy-phone").addEventListener("click", function () {
 	store.dispatch(buyPhone());
+});
+document.getElementById("buy-tablet").addEventListener("click", function () {
+	store.dispatch(buyTablet());
 });
 
 //Listener
 store.subscribe(() => {
 	console.log("Mon nouveau Store", store.getState());
 	availablePhones.innerHTML = store.getState().phones;
+	availableTablets.innerHTML = store.getState().tablets;
 });
