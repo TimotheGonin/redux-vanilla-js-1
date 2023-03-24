7f6eb5d8779c84272346baf3d49fe5a925f7ef75
TimotheGonin <gonin.timothe@gmail.com>
2023-03-24 11:15:29 +0100

    combine reducers

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const BUY_PHONE = "BUY_PHONE";
 const BUY_TABLET = "BUY_TABLET";
+const BUY_TV = "BUY_TV";
 
 function buyPhone() {
 	return {
@@ -14,16 +15,24 @@ function buyTablet() {
 		type: BUY_TABLET,
 	};
 }
+function buyTv() {
+	return {
+		type: BUY_TV,
+	};
+}
 
 //REDUCER
 // (prevState, action) => newState
 
-const initialState = {
+const initialStatePhones = {
 	phones: 5,
 	tablets: 10,
 };
+const initialStateTv = {
+	tv: 20,
+};
 
-const reducer = (state = initialState, action) => {
+const phonesReducer = (state = initialStatePhones, action) => {
 	switch (action.type) {
 		case BUY_PHONE:
 			return {
@@ -39,16 +48,36 @@ const reducer = (state = initialState, action) => {
 			return state;
 	}
 };
+const tvReducer = (state = initialStateTv, action) => {
+	switch (action.type) {
+		case BUY_TV:
+			return {
+				...state,
+				tv: state.tv - 1,
+			};
+		default:
+			return state;
+	}
+};
+
+//Combine Reducers
+const rootReducer = Redux.combineReducers({
+	phone: phonesReducer,
+	tv: tvReducer,
+});
 
 //STORE
 //Créer le STORE
-const store = Redux.createStore(reducer);
+const store = Redux.createStore(rootReducer);
 
 //Récupérer la data du Store
 const availablePhones = document.getElementById("count");
 const availableTablets = document.getElementById("count-tab");
-availablePhones.innerHTML = store.getState().phones;
-availableTablets.innerHTML = store.getState().tablets;
+const availableTv = document.getElementById("count-tv");
+availablePhones.innerHTML = store.getState().phone.phones;
+availableTablets.innerHTML = store.getState().phone.tablets;
+availableTv.innerHTML = store.getState().tv.tv;
+console.log("Initial State", store.getState());
 
 //Effectuer le Dispatch d'un action
 document.getElementById("buy-phone").addEventListener("click", function () {
@@ -57,10 +86,14 @@ document.getElementById("buy-phone").addEventListener("click", function () {
 document.getElementById("buy-tablet").addEventListener("click", function () {
 	store.dispatch(buyTablet());
 });
+document.getElementById("buy-tv").addEventListener("click", function () {
+	store.dispatch(buyTv());
+});
 
 //Listener
 store.subscribe(() => {
-	console.log("Mon nouveau Store", store.getState());
-	availablePhones.innerHTML = store.getState().phones;
-	availableTablets.innerHTML = store.getState().tablets;
+	availablePhones.innerHTML = store.getState().phone.phones;
+	availableTablets.innerHTML = store.getState().phone.tablets;
+	availableTv.innerHTML = store.getState().tv.tv;
+	console.log("Updated State", store.getState());
 });
